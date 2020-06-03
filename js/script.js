7dcb545107de92a00bbfd56b06c3f9de37fd9b89
lindam <linda.minotti2@gmail.com>
2020-06-03 23:36:42 +0200

    Campominato: completato.

--- js/script.js
+++ js/script.js
@@ -9,42 +9,54 @@
 
 
 // Creare array di numeri vietati
-var numeriVietati = [];
+var arrayNumeriVietati = [];
 
 // Ciclo per popolare array di 16 numeri vietati diversi tra loro
 var i = 0;
 while (i < 16) {
   var numeroVietato = numeriCasuali();
 
-  if (!(numeriVietati.includes(numeroVietato))) {
-    numeriVietati.push(numeroVietato);
+  if (!(arrayNumeriVietati.includes(numeroVietato))) {
+    arrayNumeriVietati.push(numeroVietato);
     i++;
   }
 }
-
-
-// for (var i = 0; i < 16; i++) {
-//
-//   numeriVietati.push(numeriCasuali());
-// }
-console.log(numeriVietati);
+console.log(arrayNumeriVietati);
 
 // Chiedere all'utente numero da 1 a 100
-// var numeroUtente = prompt('Dammi un numero da 1 a 100');
-// console.log(numeroUtente);
+var numeroUtente = parseInt(prompt('Dammi un numero da 1 a 100'));
+console.log(numeroUtente);
 
 // Confronto numeroUtente con l'array dei numeri vietati
+var numeroPresente = controllaLista(arrayNumeriVietati,numeroUtente);
+var listaNumeriValidi = [];
 
+var j = 0;
+while (j < 84 && !numeroPresente) {
+  listaNumeriValidi.push(numeroUtente);
+  numeroUtente = parseInt(prompt('Vai avanti: dammi un altro numero da 1 a 100'));
+  numeroPresente = controllaLista(arrayNumeriVietati,numeroUtente);
+  console.log(numeroUtente);
+}
+// Comunicare il punteggio
+console.log( 'Hai perso: hai azzeccato ' + listaNumeriValidi.length  + ' numeri validi!!');
 
-
-
-
-
-// Funzioni
+// FUNZIONI
 
 // Funzione per creare numeri casuali da 1 a 100
 // Ritorna un numero casuale da 1 a 100
 function numeriCasuali() {
   var numeroCasuale = Math.floor(Math.random() * 100) + 1;
   return numeroCasuale;
+}
+
+// Funzione che controlla se un numero è presente in una lista di numeri
+// Argomenti: lista di numeri e numero da controllare
+// Ritorna: booleano vero, se il numero è presente
+function controllaLista(lista, numero) {
+  for (var i = 0; i < lista.length; i++) {
+    if (lista[i] === numero) {
+      return true;
+    }
+  }
 }
