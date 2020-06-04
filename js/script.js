657f1923fe6e0d625e69489d339928ecbe4a5155
lindam <linda.minotti2@gmail.com>
2020-06-04 16:00:15 +0200

    Create funzioni per creare array di bombe.

--- js/script.js
+++ js/script.js
@@ -9,50 +9,17 @@
 // BONUS: all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
 // Con difficoltà 0=> tra 1 e 100, con difficoltà 1 => tra 1 e 80, con difficoltà 2=> tra 1 e 50
 
-
-// // Chiedere all'utente numero da 1 a 100
+// Chiedere all'utente numero da 1 a 100
 // var numeroUtente = parseInt(prompt('Dammi un numero da 1 a 100'));
 // console.log(numeroUtente);
 
 
-// Chieder all'utente un livello di difficoltà
-var difficolta = prompt('Dimmi quanto la difficoltà: 0, 1 o 2?');
-console.log(difficolta);
-
-// Creare array di numeri vietati in base alla difficoltà
-// Usa la funzione per creare numeri random tra due numeri compresi
-var arrayNumeriVietati = [];
-var i = 0;
-while (i < 16) {
-  if (difficolta == 0) {
-    var numeroVietato = numeriCasuali(1, 100);
-  } else if (difficolta == 1) {
-    var numeroVietato = numeriCasuali(1, 80);
-  } else if (difficolta == 2) {
-    var numeroVietato = numeriCasuali(1, 50);
-  }
-
-  if (!(arrayNumeriVietati.includes(numeroVietato))) {
-    arrayNumeriVietati.push(numeroVietato);
-    i++;
-  }
-}
-console.log(arrayNumeriVietati);
-
-
-// Ciclo per popolare array di 16 numeri vietati diversi tra loro
-// Usa la funzione per creare numeri cauali da 1 a 100
-// var i = 0;
-// while (i < 16) {
-//   var numeroVietato = numeriCasuali(1, 100);
-//
-//   if (!(arrayNumeriVietati.includes(numeroVietato))) {
-//     arrayNumeriVietati.push(numeroVietato);
-//     i++;
-//   }
-// }
-// console.log(arrayNumeriVietati);
-
+// Il computer deve generare 16 numeri casuali tra 1 e 100
+// rappresentanti un array di bombe
+// usa una funzione per creare l'array di numeri casuali non ripetuti
+var numeroBombe = 5; // TODO: da mettere uguale a 16
+var rangeMaxNumeriRandom = 5; // TODO: da mettere uguale a 100
+var arrayBombe = creaArrayBombe(numeroBombe, rangeMaxNumeriRandom);
 
 
 // // Confronto numeroUtente con l'array dei numeri vietati
@@ -72,21 +39,32 @@ console.log(arrayNumeriVietati);
 
 // FUNZIONI
 
-// Funzione che produce numri random tra du valori con estremi inclusi
-// Argomenti: i due estremi
-function numeriCasuali(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Il max è incluso e il min è incluso
+// Funzione che crea l'array di bombe
+// Argomenti: numero di bombe e range numeri random
+// Restituisce un array di numeri casuali rappresentanti le bombe
+function creaArrayBombe(numeroBombe, rangeMaxNumeriRandom) {
+  var arrayBombe = [];
+
+  while (arrayBombe.length < numeroBombe) {
+    var numeroCasuale = Math.floor(Math.random() * rangeMaxNumeriRandom) + 1;
+    var numeroPresente = controllaLista(arrayBombe, numeroCasuale);
+    if (!numeroPresente) {
+      arrayBombe.push(numeroCasuale);
+    }
+  }
+  return arrayBombe;
 }
+
 
 // Funzione che controlla se un numero è presente in una lista di numeri
 // Argomenti: lista di numeri e numero da controllare
 // Ritorna: booleano vero, se il numero è presente
-function controllaLista(lista, numero) {
+function controllaLista(lista, numeroDaControllare) {
+  var numeroPresente = false;
   for (var i = 0; i < lista.length; i++) {
-    if (lista[i] === numero) {
-      return true;
+    if (lista[i] === numeroDaControllare) {
+      numeroPresente = true;
     }
   }
+  return numeroPresente;
 }
